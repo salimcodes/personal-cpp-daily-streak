d2a665f499ca637da1f99237153cdc55e41b1729
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2021-08-19 12:22:23 +0100

    Updated

--- Day09.cpp
+++ Day09.cpp
@@ -6,15 +6,14 @@ using namespace std;
 int main()
 {
     int i;
-    for (i=1; i <= 20; ++i)
+    for (i=1; i <= 12; ++i)
         {
             int j;
             cout << " " << endl;
-            for(j=1; j <=20; ++j)
+            for(j=1; j <=12; ++j)
                 {
-                    cout << i*j << " ";
+                    cout << i*j << "\t ";
                 }
         }
 
 }
-
