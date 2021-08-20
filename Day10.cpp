42e71d1f90d61eaac90a55008f575fdd7164416e
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2021-08-20 10:06:45 +0100

    Create Day10.cpp
    Maximum of Two Numbers

---
+++ Day10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/* Write a complete C++ Program that has a main function, a GetTwoNumbers
+function and a FindBigOne function. The main calls the GetTwoNumbers functions,
+which asks the user for two integers.
+Then the program sends the two int's to FindBigOne, which returns the larger one
+to main.
+
+For example, if the user puts in 19 and 2, FindTheBigOne will return 19. If the two
+numbers are the same, it will send back either one. Incorporate a loop in the main
+function so that the user can keep entering two values as long as he or she desires
+*/
+int main()
+{
+    cout << "No function has been inputed yet!" << endl;
+    return 0;
+}
